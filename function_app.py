adb6e4aaf1de6154a998fcd98cadaa84792ee50f
Umasankar <Umasankarpuligundla@gmail.com>
2024-07-17 16:49:09 -0400

    All Packed. Ready to shoot out123

--- function_app.py
+++ function_app.py
@@ -1,19 +1,15 @@
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.semconv.resource import ResourceAttributes
-
 from dynatrace.opentelemetry.tracing.api import configure_dynatrace
+import azure.functions as func
+import logging
+from dynatrace.opentelemetry.azure.functions import wrap_handler
 
 tracer_provider = configure_dynatrace(
     resource=Resource.create({"my.resource.attribute": "My Resource"})
 )
-
-#Importing other modules
-
-import azure.functions as func
-import logging
-
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
-
+@wrap_handler
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -31,6 +27,6 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Hence Python Demo Accomplished. Thanks team for giving such an nice task",
+             "This HTTP triggered function executed successfully. Hence Python Demo Accomplished. Thanks team for giving such an nice test task",
              status_code=200
         )
